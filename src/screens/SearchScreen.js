aa72f4debd78f7b490221b7103f2a7540c0876b4
Gloria <jiejunguo@gmail.com>
2020-01-22 09:47:15 -0800

    Build Search Bar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
   return (
     <View>
-      <Text>Search Screen</Text>
+      <SearchBar />
+      <Text>Search Screen 2 </Text>
     </View>
   );
 };
